7ea15b2920a12e1465c5015bb2092dd8049552f5
N00rAhmed <tronn232003@gmail.com>
2024-02-12 12:05:51 +0000

    made changes which currently dont work

--- Elanco-API/Models/AnimalActivity.cs
+++ Elanco-API/Models/AnimalActivity.cs
@@ -1,23 +1,55 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Elanco_API.Models
 {
     public class AnimalActivity
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
+
+
+/*        [Key]
         public string Id { get; set; }
+*/
+        [Column("DogID")]
+        public string DogID { get; set; }
 
-        public string Name { get; set; }
+        [Column("Weight(kg)")]
+        public double WeightKG { get; set; }
 
-        [BsonRepresentation(BsonType.Double, AllowTruncation = true)]
-        public double Price { get; set; }
+        public string Date { get; set; }
 
-        public string Category { get; set; }
+        public int Hour { get; set; }
 
-        public string Author { get; set; }
+        [Column("BehaviourPattern")]
+        public string BehaviourPattern { get; set; }
+
+        [Column("ActivityLevel(steps)")]
+        public int ActivityLevelSteps { get; set; }
+
+        [Column("HeartRate(bpm)")]
+        public int HeartRateBpm { get; set; }
+
+        [Column("CalorieBurn")]
+        public double CalorieBurn { get; set; }
+
+        [Column("Temperature(C)")]
+        public double TemperatureC { get; set; }
+
+        [Column("FoodIntake(calories)")]
+        public int FoodIntakeCalories { get; set; }
+
+        [Column("WaterIntake(ml)")]
+        public int WaterIntakeMl { get; set; }
+
+        [Column("BreathingRate(breaths/min)")]
+        public int BreathingRateBreathsPerMin { get; set; }
+
+        [Column("BarkingFrequency")]
+        public string BarkingFrequency { get; set; }
+
+
 
 
     }
--- Elanco-API/Services/Http_Requests.cs
+++ Elanco-API/Services/Http_Requests.cs
@@ -5,10 +5,10 @@ namespace Elanco_API.Services
     public interface Http_Requests
     {
         List<AnimalActivity> Get();
-        AnimalActivity Get(string id);
+/*        AnimalActivity Get(string id);
         AnimalActivity Create(AnimalActivity animal_activity_data);
         void Update(string id, AnimalActivity animal_activity_data);
         void Remove(string id);
-
+*/
     }
 }
